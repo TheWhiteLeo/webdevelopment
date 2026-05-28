f5e9f43a5f275c050e655529b92b93bc719fbd10
TheWhiteLeo <kovalchuka7105@gmail.com>
2026-05-28 11:18:36 +0000

    feat: implement admin controller

---
+++ blog/app/Http/Controllers/Api/Blog/Admin/CategoryController.php
@@ -0,0 +1,117 @@
+<?php
+
+namespace App\Http\Controllers\Api\Blog\Admin;
+
+use Illuminate\Http\Request;
+use App\Models\BlogCategory;
+use Illuminate\Support\Str;
+
+class CategoryController extends BaseController
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return BlogCategory::paginate(5);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $data = $request->all();
+
+        if (empty($data["slug"])) {
+            $data["slug"] = Str::slug($data["title"]);
+        }
+
+        $item = BlogCategory::create($data);
+
+        if ($item) {
+            return [
+                "success" => true,
+                "message" => "Успішно створено"
+            ];
+        } else {
+            return [
+                "success" => false,
+                "message" => "Помилка збереження"
+            ];
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        $item = BlogCategory::find($id);
+
+        if (empty($item)) {
+            return ["message" => "Запис id=[{$id}] не знайдено"];
+        }
+
+        return $item;
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        $item = BlogCategory::find($id);
+        if (empty($item)) { //якщо ід не знайдено
+            return ["message" => "Запис id=[{$id}] не знайдено"];
+        }
+
+        $data = $request->all();
+        if (empty($data["slug"])) {
+            $data["slug"] = Str::slug($data["title"]);
+        }
+
+        $result = $item->update($data);
+
+        if ($result) {
+            return [
+                "success" => true,
+                "message" => "Успішно збережено"
+            ];
+        } else {
+            return [
+                "success" => false,
+                "message" => "Помилка збереження"
+            ];
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        $item = BlogCategory::find($id);
+
+        if (empty($item)) {
+            return [
+                "success" => false,
+                "message" => "Запис id=[{$id}] не знайдено"
+            ];
+        }
+
+        $result = $item->delete();
+
+        if ($result) {
+            return [
+                "success" => true,
+                "message" => "Успішно видалено"
+            ];
+        } else {
+            return [
+                "success" => false,
+                "message" => "Помилка видалення"
+            ];
+        }
+    }
+}
