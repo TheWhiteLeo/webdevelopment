96fbc77e0022f177301ed4c372a6abdd38181cb8
ThePolarLeo <87939684+TheWhiteLeo@users.noreply.github.com>
2026-05-29 11:51:27 +0000

    feat: implement Lab8 - Post Managing

--- blog/routes/api.php
+++ blog/routes/api.php
@@ -1,17 +1,13 @@
 <?php
 
 use App\Http\Controllers\Api\Blog\Admin\CategoryController;
-use App\Http\Controllers\Api\Blog\PostController;
+use App\Http\Controllers\Api\Blog\Admin\PostController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
-
-Route::group([ 'namespace' => 'App\Http\Controllers\Api\Blog', 'prefix' => 'blog'], function () {
-    Route::apiResource('posts', PostController::class)->names('blog.posts');
-});
 
 $groupData = [
     'namespace' => 'App\Http\Controllers\Api\Blog\Admin',
@@ -23,4 +19,8 @@ Route::group($groupData, function () {
     Route::apiResource('categories', CategoryController::class)
         ->only($methods)
         ->names('blog.admin.categories');
+
+    Route::apiResource('posts', PostController::class)
+        ->except(['show'])                               //не робити маршрут для метода show
+        ->names('blog.admin.posts');
 });
